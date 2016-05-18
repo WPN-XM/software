08677f07b8c9462224f36579ebb41f1e17a804d9
jakoch <jakoch@web.de>
2016-05-19 00:58:59 +0200

    added stub for PHP QA issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ php-qa/scripts/webinterface/Php_qa.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for PHP QA x86
+ */
+class Php_qa extends SoftwareBase
+{
+    public $name = 'PHP QA x86';
+
+    public $registryName = 'php-qa';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\php-qa';
+
+    public $files = [
+        '\bin\php-qa\php-qa.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
