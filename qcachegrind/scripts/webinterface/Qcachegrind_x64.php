5e844123995b4f92dd5cd5e9b4e41544654a3d6b
jakoch <jakoch@web.de>
2016-05-19 00:59:44 +0200

    added stub for QCachegrind issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ qcachegrind/scripts/webinterface/Qcachegrind_x64.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for QCacheGrind x64
+ */
+class Qcachegrind_x64 extends SoftwareBase
+{
+    public $name = 'QCacheGrind x64';
+
+    public $registryName = 'qcachegrind-x64';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\qcachegrind';
+
+    public $files = [
+        '\bin\qcachegrind\qcachegrind-x64.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
