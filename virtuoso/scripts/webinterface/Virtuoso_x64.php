79d08aabce3e020c078cdb72e827c4615abebd81
jakoch <jakoch@web.de>
2016-05-19 01:03:09 +0200

    added stub for Virtuoso issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ virtuoso/scripts/webinterface/Virtuoso_x64.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for Virtuoso x64
+ */
+class Virtuoso_x64 extends SoftwareBase
+{
+    public $name = 'Virtuoso x64';
+
+    public $registryName = 'virtuoso-x64';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\virtuoso';
+
+    public $files = [
+        '\bin\virtuoso\virtuoso-x64.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
