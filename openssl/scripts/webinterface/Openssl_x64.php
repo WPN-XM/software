bd0b55f49830db70260e529d02a92afd4545de58
jakoch <jakoch@web.de>
2016-05-19 01:07:41 +0200

    added stub for OpenSSL issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ openssl/scripts/webinterface/Openssl_x64.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for OpenSSL (64-bit)
+ */
+class Openssl_x64 extends SoftwareBase
+{
+    public $name = 'OpenSSL (64-bit)';
+
+    public $registryName = 'openssl-x64';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\openssl';
+
+    public $files = [
+        '\bin\openssl\openssl-x64.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
