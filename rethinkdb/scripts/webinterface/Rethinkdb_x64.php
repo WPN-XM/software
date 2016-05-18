df272b31b6a2ff47934f353d6dcfd2aa6916c385
jakoch <jakoch@web.de>
2016-05-19 01:00:27 +0200

    added stub for RethinkDB issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ rethinkdb/scripts/webinterface/Rethinkdb_x64.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for RethinkDB
+ */
+class Rethinkdb_x64 extends SoftwareBase
+{
+    public $name = 'RethinkDB';
+
+    public $registryName = 'rethinkdb-x64';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\rethinkdb';
+
+    public $files = [
+        '\bin\rethinkdb\rethinkdb-x64.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
