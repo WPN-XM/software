6ef2eb769dd00e1e9fcd8c68017c380c056a6f53
jakoch <jakoch@web.de>
2016-05-19 00:38:56 +0200

    added adminer issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ adminer/scripts/webinterface/Adminer.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for Adminer
+ */
+class Adminer extends SoftwareBase
+{
+    public $name = 'Adminer';
+
+    public $registryName = 'adminer';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\adminer';
+
+    public $files = [
+        '\bin\adminer\adminer.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
