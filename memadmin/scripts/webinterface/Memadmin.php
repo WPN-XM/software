5ba3dc961753ebde235d0296ed9870dca936f561
jakoch <jakoch@web.de>
2016-05-19 00:55:29 +0200

    added stub for Memadmin issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ memadmin/scripts/webinterface/Memadmin.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for Memadmin
+ */
+class Memadmin extends SoftwareBase
+{
+    public $name = 'Memadmin';
+
+    public $registryName = 'memadmin';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\memadmin';
+
+    public $files = [
+        '\bin\memadmin\memadmin.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
