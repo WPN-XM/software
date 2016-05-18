795972e2b8b8d3b1cfb86d7d89a1771f2d8864de
jakoch <jakoch@web.de>
2016-05-19 00:40:14 +0200

    added stub for apache_benchmark issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ apache-benchmark/scripts/webinterface/Apache_benchmark.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for Apache Benchmark (ab)
+ */
+class Apache_benchmark extends SoftwareBase
+{
+    public $name = 'Apache Benchmark (ab)';
+
+    public $registryName = 'apache-benchmark';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\apache-benchmark';
+
+    public $files = [
+        '\bin\apache-benchmark\apache-benchmark.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
