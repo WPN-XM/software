8ae8a226b9c9e258c8745e3d447f4d76fc94b5c4
jakoch <jakoch@web.de>
2016-05-19 01:02:05 +0200

    added stub for Sphinx issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ sphinx/scripts/webinterface/Sphinx.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for Sphinx Search x86
+ */
+class Sphinx extends SoftwareBase
+{
+    public $name = 'Sphinx Search x86';
+
+    public $registryName = 'sphinx';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\sphinx';
+
+    public $files = [
+        '\bin\sphinx\sphinx.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
