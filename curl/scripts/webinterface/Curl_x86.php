58a0befb099877bdf8e42791c3fe4170e6b2645d
jakoch <jakoch@web.de>
2016-05-19 00:53:38 +0200

    added stub for curl issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ curl/scripts/webinterface/Curl_x86.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for cURL x86
+ */
+class Curl_x86 extends SoftwareBase
+{
+    public $name = 'cURL x86';
+
+    public $registryName = 'curl-x86';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\curl';
+
+    public $files = [
+        '\bin\curl\curl-x86.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
