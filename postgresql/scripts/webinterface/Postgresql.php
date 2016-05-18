c7a8294407e17ec459adc8f8db5739fc198953d4
jakoch <jakoch@web.de>
2016-05-19 00:59:24 +0200

    added stub for PostgreSQL issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ postgresql/scripts/webinterface/Postgresql.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for PostgreSQL x86
+ */
+class Postgresql extends SoftwareBase
+{
+    public $name = 'PostgreSQL x86';
+
+    public $registryName = 'postgresql';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\postgresql';
+
+    public $files = [
+        '\bin\postgresql\postgresql.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
