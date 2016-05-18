a36dc85f5c4c2a333955704c23b2c42fbb9f481c
jakoch <jakoch@web.de>
2016-05-19 00:57:07 +0200

    added stub for Node/npm issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ nodenpm/scripts/webinterface/Nodenpm.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for NodeNPM
+ */
+class Nodenpm extends SoftwareBase
+{
+    public $name = 'NodeNPM';
+
+    public $registryName = 'nodenpm';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\nodenpm';
+
+    public $files = [
+        '\bin\nodenpm\nodenpm.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
