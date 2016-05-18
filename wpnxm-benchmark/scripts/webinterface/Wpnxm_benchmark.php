c1308de3a96e02d8348e396d366dbfc28eeefc6c
jakoch <jakoch@web.de>
2016-05-19 01:04:12 +0200

    added stub for WPNXM Benchmark issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ wpnxm-benchmark/scripts/webinterface/Wpnxm_benchmark.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+
+/**
+ * WPN-XM Webinterface - Class for WPN-XM Benchmark
+ */
+class Wpnxm_benchmark extends SoftwareBase
+{
+    public $name = 'WPN-XM Benchmark';
+
+    public $registryName = 'wpnxm-benchmark';
+
+    public $installationFolder = /* WPNXM_ROOT . */ '\bin\wpnxm-benchmark';
+
+    public $files = [
+        '\bin\wpnxm-benchmark\wpnxm-benchmark.exe',
+    ];
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
