b6836f5cf82aa5e7d190c4769fc5851dec7f22e1
jakoch <jakoch@web.de>
2016-05-19 00:40:27 +0200

    added stub for arangodb issue https://github.com/WPN-XM/WPN-XM/issues/549

---
+++ arangodb/scripts/tasks/Install.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
+
+namespace WPNXM\Software\Arangodb\Task;
+
+use WPNXM\TaskRunner\TaskBase;
+
+class Install extends TaskBase
+{
+
+    public function __invoke()
+    {
+        echo __CLASS__ . ' invoked';
+    }
+}
