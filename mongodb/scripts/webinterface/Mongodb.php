b2eb6aa89203bf237c340f7a75290be7f58871b8
jakoch <jakoch@web.de>
2016-05-23 10:19:11 +0200

    MongoDB - moved from "webinterface" to "software" issue https://github.com/WPN-XM/WPN-XM/issues/568

--- mongodb/scripts/webinterface/Mongodb.php
+++ mongodb/scripts/webinterface/Mongodb.php
@@ -20,16 +20,61 @@ class Mongodb extends SoftwareBase
 {
     public $name = 'MongoDB';
 
-    public $registryName = 'mongodb';
-
-    public $installationFolder = /* WPNXM_ROOT . */ '\bin\mongodb';
+    public $installationFolder = '\bin\mongodb';
 
     public $files = [
-        '\bin\mongodb\mongodb.exe',
+        '\bin\mongodb\mongodb.conf',
+        '\bin\mongodb\bin\mongod.exe',
     ];
+
+    public $configFile = '\bin\mongodb\mongodb.conf';
 
     public function __invoke()
     {
         echo __CLASS__ . ' invoked';
+    }
+
+    public function getVersion()
+    {
+        if ($this->isInstalled() === false) {
+            return 'not installed';
+        }
+
+        if (!extension_loaded('mongo')) {
+            return \Webinterface\Helper\Serverstack::printExclamationMark(
+                'The PHP Extension "Mongo" is required.'
+            );
+        }
+
+        try {
+            $m = new \MongoClient();
+
+            //require admin privilege
+            $db = $m->admin;
+
+            //$mongodb_info = $db->command(['buildinfo' => true]);
+            //$mongodb_version = $mongodb_info['version'];
+
+            // this returns an array with the keys "retval","ok"
+            $mongodb_version = $db->execute('return db.version()');
+        } catch (\MongoConnectionException $e) {
+            return \Webinterface\Helper\Serverstack::printExclamationMark(
+                    $e->getMessage().'. Please wake the daemon.'
+            );
+        }
+
+        return $mongodb_version['retval'];
+    }
+
+    public function getPassword()
+    {
+        $ini = new \Webinterface\Helper\INIReaderWriter(WPNXM_INI);
+
+        return $ini->get('MongoDB', 'password');
+    }
+
+    public function setPassword($password)
+    {
+        return 'Not implemented, yet.';
     }
 }
