35913ceb72b2b9579574020c04e191ca43689527
jakoch <jakoch@web.de>
2016-05-23 10:25:22 +0200

    phpMyAdmin - moved from "webinterface" to "software" issue https://github.com/WPN-XM/WPN-XM/issues/568

--- phpmyadmin/scripts/webinterface/Phpmyadmin.php
+++ phpmyadmin/scripts/webinterface/Phpmyadmin.php
@@ -22,14 +22,53 @@ class Phpmyadmin extends SoftwareBase
 
     public $registryName = 'phpmyadmin';
 
-    public $installationFolder = /* WPNXM_ROOT . */ '\bin\phpmyadmin';
+    public $installationFolder = '\www\tools\phpmyadmin';
 
     public $files = [
-        '\bin\phpmyadmin\phpmyadmin.exe',
+        '\www\tools\phpmyadmin\libraries\Config.class.php',
+        '\www\tools\phpmyadmin\index.php',
     ];
 
     public function __invoke()
     {
         echo __CLASS__ . ' invoked';
+    }
+
+    public function getVersion()
+    {
+        if ($this->isInstalled() === false) {
+            return 'not installed';
+        }
+
+        $file = WPNXM_DIR.$this->files[0];
+
+        $maxLines = 120; // read only the first few lines of the file
+
+        $file_handle = fopen($file, 'r');
+
+        for ($i = 0; $i < $maxLines && !feof($file_handle); $i++) {
+            $line = fgets($file_handle, 1024);
+            // lets grab the version from this line:
+            // $this->set('PMA_VERSION', '4.0.0-beta1');
+            preg_match("#PMA_VERSION', '(.*)'#", $line, $matches);
+
+            if (isset($matches[0])) {
+                break;
+            }
+        }
+        fclose($file_handle);
+
+        return $matches[1];
+    }
+
+    public static function getLink()
+    {
+        // is phpmyadmin installed?
+       if (is_dir(WPNXM_WWW_DIR.'phpmyadmin') === true) {
+           $password = \Webinterface\Helper\Serverstack::getPassword('mariadb');
+           $href     = WPNXM_ROOT.'tools/phpmyadmin/index.php?lang=en&server=1&pma_username=root&pma_password='.$password;
+
+           return '<a href="'.$href.'">phpMyAdmin</a>';
+       }
     }
 }
