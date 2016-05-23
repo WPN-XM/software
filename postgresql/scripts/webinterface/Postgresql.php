6b7772e1511203c3a676dde8f85c32ea6c543a17
jakoch <jakoch@web.de>
2016-05-23 10:28:21 +0200

    postgresql - moved from "webinterface" to "software" issue https://github.com/WPN-XM/WPN-XM/issues/568

--- postgresql/scripts/webinterface/Postgresql.php
+++ postgresql/scripts/webinterface/Postgresql.php
@@ -22,14 +22,42 @@ class Postgresql extends SoftwareBase
 
     public $registryName = 'postgresql';
 
-    public $installationFolder = /* WPNXM_ROOT . */ '\bin\postgresql';
+    /**
+     * Init:    initdb.exe <datafolderpath>
+     * Start:   pg_ctl.exe -D "<datafolderpath>" -l logfile start
+     */
+    public $installationFolder = '\bin\postgresql';
 
     public $files = [
-        '\bin\postgresql\postgresql.exe',
+        // Note: the folder was renamed from "pgsql" (name in the original zip) to "postgresql"
+        '\bin\postgresql\bin\initdb.exe',
+        '\bin\postgresql\bin\postgresql.conf',
+        '\bin\postgresql\bin\pg_ctl.exe', // http://www.postgresql.org/docs/9.3/static/app-pg-ctl.html
     ];
+
+    public $configFile = '\bin\postgresql\bin\postgresql.conf';
 
     public function __invoke()
     {
         echo __CLASS__ . ' invoked';
+    }
+
+     /**
+     * Returns Version.
+     *
+     * @return string Version
+     */
+    public function getVersion()
+    {
+        if ($this->isInstalled(true) === false) {
+            return 'not installed';
+        }
+
+        $command = 'start \bin\postgresql\bin\pgsql.exe -V';
+        exec($command, $output);
+
+        $output = str_replace('pgsql (PostgreSQL)', '', $output);
+
+        return $output;
     }
 }
