9e65b1cdeaf18f39be405a5811d92426d93ef1aa
jakoch <jakoch@web.de>
2016-05-23 10:21:17 +0200

    Nginx - moved from "webinterface" to "software" issue https://github.com/WPN-XM/WPN-XM/issues/568

--- nginx/scripts/webinterface/Nginx.php
+++ nginx/scripts/webinterface/Nginx.php
@@ -22,14 +22,40 @@ class Nginx extends SoftwareBase
 
     public $registryName = 'nginx';
 
-    public $installationFolder = /* WPNXM_ROOT . */ '\bin\nginx';
+    public $installationFolder = '\bin\nginx';
 
     public $files = [
-        '\bin\nginx\nginx.exe',
+        '\bin\nginx\conf\nginx.conf',
+        '\bin\nginx\bin\nginx.exe',
     ];
+
+    public $configFile = '\bin\nginx\conf\nginx.conf';
 
     public function __invoke()
     {
         echo __CLASS__ . ' invoked';
+    }
+
+    /**
+     * Returns Version.
+     *
+     * @return string Version
+     */
+    public function getVersion()
+    {
+        if (strpos($_SERVER['SERVER_SOFTWARE'], 'Apache') !== false) {
+            return \Webinterface\Helper\Serverstack::printExclamationMark('Traitor - you are using Apache!');
+        }
+
+        if (strpos($_SERVER['SERVER_SOFTWARE'], 'Development Server') !== false) {
+            return \Webinterface\Helper\Serverstack::printExclamationMark('The webinterface is served via the embedded PHP Development Server!');
+        }
+
+        return substr($_SERVER['SERVER_SOFTWARE'], 6);
+    }
+
+    public function install()
+    {
+        $this->unzip($file, $installationFolder);
     }
 }
