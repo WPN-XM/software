a981aef0c3591ebb4f7a6f282619f8f9bdf5979e
jakoch <jakoch@web.de>
2016-05-23 10:13:21 +0200

    Adminer - moved from "webinterface" to "software" issue https://github.com/WPN-XM/WPN-XM/issues/568

--- adminer/scripts/webinterface/Adminer.php
+++ adminer/scripts/webinterface/Adminer.php
@@ -22,14 +22,54 @@ class Adminer extends SoftwareBase
 
     public $registryName = 'adminer';
 
-    public $installationFolder = /* WPNXM_ROOT . */ '\bin\adminer';
+    public $installationFolder = /* WPNXM_ROOT . */ '\www\adminer';
 
     public $files = [
-        '\bin\adminer\adminer.exe',
+        '\www\tools\adminer\adminer.php',
     ];
 
     public function __invoke()
     {
         echo __CLASS__ . ' invoked';
     }
+    
+    /**
+     * Returns PHP Version.
+     *
+     * @return string PHP Version
+     */
+    public function getVersion()
+    {
+        if ($this->isInstalled() === false) {
+            return 'not installed';
+        }
+
+        $file = WPNXM_DIR.$this->files[0];
+
+        $maxLines = 8; // read only the first few lines of the file
+
+        $file_handle = fopen($file, 'r');
+
+        for ($i = 0; $i < $maxLines && !feof($file_handle); $i++) {
+            $line = fgets($file_handle, 1024);
+            // lets grab the version from the phpdoc tag
+            preg_match('/\* \@version (\d+.\d+.\d+)/', $line, $matches);
+
+            if (isset($matches[0])) {
+                break;
+            }
+        }
+        fclose($file_handle);
+
+        return $matches[1];
+    }
+
+    public static function getLink()
+    {
+        // is adminer installed?
+        if (is_dir(WPNXM_WWW_DIR.'tools/adminer') === true) {
+            return '<a href="'.WPNXM_ROOT.'tools/adminer/adminer.php?server=localhost&amp;username=root">Adminer</a>';
+        }
+    }
+
 }
