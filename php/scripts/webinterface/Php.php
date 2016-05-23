a25d0bf3c2dde1813b02cc2f7db5eb5c386bee4c
jakoch <jakoch@web.de>
2016-05-23 10:32:07 +0200

    PHP - moved from "webinterface" to "software" issue https://github.com/WPN-XM/WPN-XM/issues/568

--- php/scripts/webinterface/Php.php
+++ php/scripts/webinterface/Php.php
@@ -22,14 +22,40 @@ class Php extends SoftwareBase
 
     public $registryName = 'php';
 
-    public $installationFolder = /* WPNXM_ROOT . */ '\bin\php';
+    public $installationFolder = '\bin\php';
 
     public $files = [
+        '\bin\php\php.ini',
         '\bin\php\php.exe',
     ];
+
+    public $configFile = '\bin\php\php.ini';
 
     public function __invoke()
     {
         echo __CLASS__ . ' invoked';
+    }
+
+    /**
+     * Returns PHP Version.
+     *
+     * @return string PHP Version
+     */
+    public function getVersion()
+    {
+        return PHP_VERSION;
+    }
+
+    public static function getPHPExtensionDirectory()
+    {
+        $phpinfo = \Webinterface\Helper\PHPInfo::getPHPInfo(true);
+
+        $extensionDir = '';
+
+        if (preg_match('/extension_dir([ =>\t]*)([^ =>\t]+)/', $phpinfo, $matches)) {
+            $extensionDir = $matches[2];
+        }
+
+        return $extensionDir;
     }
 }
