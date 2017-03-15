6d98ff2d1fb58f703ecf66d20ef95304b27833ea
jakoch <jakoch@web.de>
2017-03-15 14:59:15 +0100

    update + moved get version logic into tasks/Version.php

--- adminer/scripts/tasks/Version.php
+++ adminer/scripts/tasks/Version.php
@@ -14,10 +14,32 @@ namespace WPNXM\Software\Adminer\Task;
 use WPNXM\TaskRunner\TaskBase;
 
 class Version extends TaskBase
-{
+{    
+    public function __construct($file)
+    {
+        $this->file = $file;
+    }
 
     public function __invoke()
     {
-        echo __CLASS__ . ' invoked';
+        $file = WPNXM_DIR . $this->file;
+
+        $maxLines = 8; // read only the first few lines of the file
+
+        $file_handle = fopen($file, 'r');
+
+        for ($i = 0; $i < $maxLines && !feof($file_handle); $i++) {
+            $line = fgets($file_handle, 1024);
+            // lets grab the version from the phpdoc tag
+            preg_match('/\* \@version (\d+.\d+.\d+)/', $line, $matches);
+
+            if (isset($matches[0])) {
+                break;
+            }
+        }
+
+        fclose($file_handle);
+
+        return $matches[1];
     }
 }
--- adminer/scripts/webinterface/Adminer.php
+++ adminer/scripts/webinterface/Adminer.php
@@ -2,7 +2,7 @@
 
 /**
  * WPИ-XM Server Stack
- * Copyright © 2010 - 2016 Jens A. Koch <jakoch@web.de>
+ * Copyright © 2010 - 2017 Jens A. Koch <jakoch@web.de>
  * http://wpn-xm.org/
  *
  * This source file is subject to the terms of the MIT license.
@@ -10,6 +10,9 @@
  */
 
 namespace WPNXM\Webinterface\Software;
+
+use WPNXM\Webinterface\SoftwareBase;
+use WPNXM\Software\Adminer\Task\Version;
 
 /**
  * WPN-XM Webinterface - Class for Adminer
@@ -42,24 +45,11 @@ class Adminer extends SoftwareBase
             return 'not installed';
         }
 
-        $file = WPNXM_DIR.$this->files[0];
+        $file = WPNXM_DIR . $this->files[0];
 
-        $maxLines = 8; // read only the first few lines of the file
+        $version = new Version($file);
 
-        $file_handle = fopen($file, 'r');
-
-        for ($i = 0; $i < $maxLines && !feof($file_handle); $i++) {
-            $line = fgets($file_handle, 1024);
-            // lets grab the version from the phpdoc tag
-            preg_match('/\* \@version (\d+.\d+.\d+)/', $line, $matches);
-
-            if (isset($matches[0])) {
-                break;
-            }
-        }
-        fclose($file_handle);
-
-        return $matches[1];
+        return $version;
     }
 
     public static function getLink()
